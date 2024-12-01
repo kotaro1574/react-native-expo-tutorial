f170fe2ee0ef7dcfd1f655cbfc810c6ba4aaf48f
kotaro1574 <shake1574@gmail.com>
2024-12-01 12:33:10 +0900

    update: Stack+Tabs Navigatoin

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { Link } from "expo-router";
+
+export default function Page() {
+  return (
+    <View>
+      <Text>Home page</Text>
+      <Link href="/about">About</Link>
+      <Link href="(tabs)">Tabs</Link>
+    </View>
+  );
+}
